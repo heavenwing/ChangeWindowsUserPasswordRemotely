4c24197dd3043fa5300a826ae733981d74428833
朱永光 <heavenwing@msn.com>
2018-01-08 17:29:57 +0800

    Add project files.

---
+++ ChangeWindowsUserPassword/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ChangeWindowsUserPassword.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChangeWindowsUserPassword.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ChangePasswordVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new PrincipalContext(ContextType.Machine))
+                using (var user = UserPrincipal.FindByIdentity(context, User.Identity.Name))
+                {
+                    user.ChangePassword(model.OldPassword, model.NewPassword);
+                }
+
+                return RedirectToAction("About");
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Password is changed!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ChangeWindowsUserPassword/ViewModels/ChangePasswordVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChangeWindowsUserPassword.ViewModels
+{
+    public class ChangePasswordVM
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "New password is not match!")]
+        public string ConfirmedPassword { get; set; }
+    }
+}
